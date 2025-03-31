552c202e7d231f2da4c66d0e6f0bd4dcd38a20bf
이재현 <lcw7010@naver.com>
2025-03-31 16:02:09 +0900

    공부한 것들 커밋

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,10 +5,14 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name,int age){
+        this(name,age,50);
+    }
     MemberConstruct(String name,int age,int grade){
         System.out.println("생성자 호출 name=" + name + "age= "+age+" grade= "+ grade);
         this.name = name;
         this.age = age;
         this.grade = grade;
+        //this(name,age,grade)
     }
 }
